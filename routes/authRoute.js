fae47f931825eb84bde733bbc42b9254b8f23525
Josimar-Victoria <josimarvictoria968@gmail.com>
2022-12-20 11:38:03 -0500

    login admin y add save address

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -14,7 +14,10 @@ const {
   logout,
   updatePassword,
   forgotPasswordToken,
-  resetPassword
+  resetPassword,
+  loginAdmin,
+  getWishlist,
+  saveAddress
 } = require('../controller/userCtrl')
 
 // Crear Un Usuario  ----------------------------------------------
@@ -22,6 +25,12 @@ router.post('/register', createUser)
 
 // Inicia sesión un usuario ----------------------------------------------
 router.post('/login', loginUser)
+
+// Inicio de sesión de administrador ----------------------------------------------
+router.post('/admin-login', loginAdmin)
+
+// Olvidé el token de contraseña ----------------------------
+router.post('/forgot-password-token', forgotPasswordToken)
 
 // Eliminar un Usuario ----------------------------------------------
 router.delete('/:id', deleteUser)
@@ -35,8 +44,14 @@ router.get('/all-users', getAllUser)
 //Manejar el token de actualización ------------------------------
 router.get('/refresh', handleRefreshToken)
 
+// obtener lista de deseos ----------------------------------------------
+router.get('/wishlist', authMiddleware, getWishlist)
+
 // Obtener un solo usuario ----------------------------------------------
 router.get('/:id', authMiddleware, isAdmin, getaUser)
+
+// Restablecer la contraseña ----------------------------------------------
+router.put('/save-address',authMiddleware, saveAddress)
 
 // Actualizar un usuario ----------------------------------------------
 router.put('/edit-user', authMiddleware, updatedUser)
@@ -49,9 +64,6 @@ router.put('/unblock-user/:id', authMiddleware, isAdmin, unblockUser)
 
 // Actualiza contraseña ------------------------------
 router.put('/password', authMiddleware, updatePassword)
-
-// Olvidé el token de contraseña ----------------------------
-router.post('/forgot-password-token', forgotPasswordToken)
 
 // Restablecer la contraseña ----------------------------------------------
 router.put('/reset-password/:token', resetPassword)
